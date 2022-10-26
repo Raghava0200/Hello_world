e6ccab876489bb697ad32a73ec38ea032e4d3249
Raghava <raghava.jaggayyagari@sap.com>
2022-10-26 10:53:20 +0530

    qwerty

---
+++ src/main/java/com/sap/first_application/controllers/MyController.java
@@ -0,0 +1,15 @@
+package com.sap.first_application.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("wishMe")
+    Object helloController()
+    {
+        return "Hello Raghava Jaggayyagari";
+    }
+
+}
